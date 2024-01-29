f3e1667b264981e01f149c68d7d25f9d93134593
unknown <guilherme_nascimeento18@outlook.com>
2024-01-29 18:54:59 -0300

    Desafio encriptador Oracle ONE T6

---
+++ script/index.js
@@ -0,0 +1,116 @@
+var btnCopiar = document.querySelector(".btn-copiar");
+
+btnCopiar.onclick = function() {
+    resultado.select();
+    document.execCommand("copy");
+    inputTexto.value = "";
+    inputTexto.focus();
+    resultado.textContent = "";
+    btnCopiar.classList.add("invisivel");
+}
+
+function codifica(texto) {
+    var caracteres = texto.value.split("");
+
+    caracteres.forEach(function(item, i) {
+        switch(item){
+            case "a": 
+            caracteres[i] = "ai";
+            break;
+            case "e": 
+            caracteres[i] = "enter";
+            break;
+            case "i": 
+            caracteres[i] = "imes";
+            break;
+            case "o": 
+            caracteres[i] = "ober";
+            break;
+            case "u": 
+            caracteres[i] = "ufat";
+            break;
+        }
+    })
+    return caracteres.join("");
+}
+
+function decodifica(texto) {
+    var codigo = [["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]];
+    texto = texto.value;
+
+    for(var i = 0; i < codigo.length; i++) {
+        if(texto.includes(codigo[i][1])) {
+            texto = texto.replaceAll(codigo[i][1], codigo[i][0]);
+        }
+    }
+    return texto;
+}
+
+function escreveCodificado() {
+    var btnCopiar = document.querySelector(".btn-copiar");
+    btnCopiar.classList.remove("invisivel");
+    
+    resultado.textContent = codifica(inputTexto);
+}
+
+function escreveDecodificado() {
+    var btnCopiar = document.querySelector(".btn-copiar");
+    btnCopiar.classList.remove("invisivel");
+
+    resultado.textContent = decodifica(inputTexto);
+}
+
+var inputTexto = document.querySelector(".input-texto");
+var resultado = document.querySelector(".resultado");
+
+var btnCodificar = document.querySelector(".btn-codificar");
+var btnDecodificar = document.querySelector(".btn-decodificar");
+
+var mensagensErro = document.querySelector(".mensagens-erro");
+
+btnCodificar.onclick = function() {
+    var erros = validaTexto(inputTexto);
+
+    if(erros.length > 0) {
+        mostraErros(erros);
+        resultado.textContent = "";
+        return;
+    }
+
+    escreveCodificado();
+    mensagensErro.innerHTML = "";
+}
+
+btnDecodificar.onclick = function() {
+    var erros = validaTexto(inputTexto);
+
+    if(erros.length > 0) {
+        mostraErros(erros);
+        resultado.textContent = "";
+        return;
+    }
+    
+    escreveDecodificado();
+    mensagensErro.innerHTML = "";
+}
+
+function validaTexto(texto) {
+    var erros = [];
+
+    if(texto.value == 0) erros.push(" Digite algum texto!");
+
+    if(/[A-Z-À-Ú-à-ú]/.test(texto.value)) erros.push(" Apenas letras minúsculas e sem acento!");
+
+    return erros;
+}
+
+function mostraErros(erros) {
+    var ul = document.querySelector(".mensagens-erro");
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
